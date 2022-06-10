9095340a59284214652286907133f0959ea8ff41
wildahnourina <wildah.nurina@gmail.com>
2022-06-10 11:27:20 +0700

    Basic Gameplay
    (izin create respository baru yg lama berantakan)

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Script/PadelController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadelController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveopedal(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+        if (Input.GetKey(upKey))
+        {
+            return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            return Vector2.down * speed;
+        }
+
+        return Vector2.zero;
+    }
+
+    private void moveopedal(Vector2 move)
+    {
+        rb.velocity = move;
+    }
+}
